9480ac9699cad3c989f9d2e4ce2cf83c1ea82c4e
Sun-2302 <hei.dera.932@gmail.com>
2024-05-07 12:08:41 +0300

    feat: initialize project

---
+++ src/main/java/school/hei/javastationmanagement/JavaStationManagementApplication.java
@@ -0,0 +1,13 @@
+package school.hei.javastationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStationManagementApplication.class, args);
+    }
+
+}
